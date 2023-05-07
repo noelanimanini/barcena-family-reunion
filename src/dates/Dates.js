3cf9c8b88057bfe260061d9537b6b5f83affa020
noelanimanini <crystalnvillanueva@gmail.com>
2023-05-06 21:55:37 -0700

    home page and dates were added

---
+++ src/dates/Dates.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './dates.css'
+
+function Dates() {
+  return (
+    <div>
+        <div>
+        July 12th - 14th, 2024 
+        </div>
+        <div>
+            Location: Naalehu and Hilo
+        </div>
+    </div>
+  )
+}
+
+export default Dates
---
+++ src/homepage/homepage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './homepage.css'
+
+function homepage() {
+  return (
+    <div className="parentDiv"><h1>Barcena Family Reunion</h1></div>
+  )
+}
+
+export default homepage
